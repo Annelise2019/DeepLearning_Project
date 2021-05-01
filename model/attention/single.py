a4fb2ed1b62974e98660f52574ce07fbf45c7074
Annelise2019 <53711263+Annelise2019@users.noreply.github.com>
2021-05-01 21:08:23 +0200

    Update single.py

--- model/attention/single.py
+++ model/attention/single.py
@@ -5,19 +5,19 @@ import torch
 import math
 
 class Attention(nn.Module):
-  """
-    Compute 'Scaled Dot Product Attention
-  """
+    """
+      Compute 'Scaled Dot Product Attention
+    """
   
-  def forward(self, query, key, value, mask=None, dropout=None):
-    scores = torch.matmul(query, key.transpose(-2, -1)) \
-             / math.sqrt(query.size(-1))
+    def forward(self, query, key, value, mask=None, dropout=None):
+        scores = torch.matmul(query, key.transpose(-2, -1)) \
+                 / math.sqrt(query.size(-1))
     
-    if mask is not None:
-      scores = scores.masked_fill(mask == 0, -1e9)
-      p_attn = F.softmax(scores, dim=-1)
+        if mask is not None:
+            scores = scores.masked_fill(mask == 0, -1e9)
+            p_attn = F.softmax(scores, dim=-1)
       
-      if dropout is not None:
-        p_attn = dropout(p_attn)
+        if dropout is not None:
+            p_attn = dropout(p_attn)
                 
-      return torch.matmul(p_attn, value), p_attn
+        return torch.matmul(p_attn, value), p_attn
