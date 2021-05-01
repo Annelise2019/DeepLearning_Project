7f9548b9b795b980c46fd8c6c1545d5338220326
Annelise2019 <53711263+Annelise2019@users.noreply.github.com>
2021-05-01 21:12:10 +0200

    Update two_stream_transformer.py

--- model/two_stream_transformer.py
+++ model/two_stream_transformer.py
@@ -10,7 +10,7 @@ from .embedding import BERTEmbedding
 from .test_transformer_spatial import BERT_Space
 
 class BERT(nn.Module):
-  def __init__(self,
+    def __init__(self,
                  in_channels=3,
                  keypoints=25, #--->to do: feature_size, d_H, dropout=0.1
                  dropout_in_embedding=0.1,
@@ -20,42 +20,42 @@ class BERT(nn.Module):
                  dropout_in_transformer=0.1,
                  data_bn=True,
                  **kwargs):    
-    super().__init__()
+        super().__init__()
     
-    self.feature_size = in_channels*keypoints
+        self.feature_size = in_channels*keypoints
     
-    #data_bn
-    if data_bn == True:
-        self.bn = nn.BatchNorm1d(self.feature_size)
+        #data_bn
+        if data_bn == True:
+            self.bn = nn.BatchNorm1d(self.feature_size)
     
-    #data position embedding for BERT
-    self.embedding = BERTEmbedding(self.feature_size,hidden_size,dropout_in_embedding) 
+        #data position embedding for BERT
+        self.embedding = BERTEmbedding(self.feature_size,hidden_size,dropout_in_embedding) 
     
-    #multi-layers transformer blocks, deep network
-    self.transformer_blocks_cla = nn.ModuleList([TransformerBlock(hidden_size, num_att_heads, hidden_size * 4, dropout_in_transformer) for _ in range(n_layers_cla)])
+        #multi-layers transformer blocks, deep network
+        self.transformer_blocks_cla = nn.ModuleList([TransformerBlock(hidden_size, num_att_heads, hidden_size * 4, dropout_in_transformer) for _ in range(n_layers_cla)])
     
     
-  def forward(self,x):
-    N, C, T, V, M = x.size()       
+    def forward(self,x):
+        N, C, T, V, M = x.size()       
         
-    x = x.permute(0, 4, 3, 1, 2).contiguous()
-    x = x.view(N * M, V * C, T)
-    x = self.bn(x) #N*M, T, V*C ---> to research
-    x = x.permute(0,2,1).contiguous()#N*M,T,V*C
+        x = x.permute(0, 4, 3, 1, 2).contiguous()
+        x = x.view(N * M, V * C, T)
+        x = self.bn(x) #N*M, T, V*C ---> to research
+        x = x.permute(0,2,1).contiguous()#N*M,T,V*C
     
-    #To add the first token
-    token = torch.ones(x.size(0),1,x.size(-1)).float().to(x.device)
-    x = torch.cat((token,x),1)
+        #To add the first token
+        token = torch.ones(x.size(0),1,x.size(-1)).float().to(x.device)
+        x = torch.cat((token,x),1)
     
-    mask_new = torch.ones(x.size()[:2]).to(x.device)            
-    mask_att = (mask_new>0).unsqueeze(1).repeat(1,x.size(1),1).unsqueeze(1) 
+        mask_new = torch.ones(x.size()[:2]).to(x.device)            
+        mask_att = (mask_new>0).unsqueeze(1).repeat(1,x.size(1),1).unsqueeze(1) 
     
-    #Embedding  x.size()=N*M,T,C*V
-    x = self.embedding(x) # embedding the indexed sequence to sequence of vectors  
+        #Embedding  x.size()=N*M,T,C*V
+        x = self.embedding(x) # embedding the indexed sequence to sequence of vectors  
     
-    # running over multiple transformer blocks
-    for transformer in self.transformer_blocks_cla:
-        x, _ = transformer.forward(x, mask_att) #x.size()=(N*M,T,H)
+        # running over multiple transformer blocks
+        for transformer in self.transformer_blocks_cla:
+            x, _ = transformer.forward(x, mask_att) #x.size()=(N*M,T,H)
         
-    return x
+        return x
     
