e9fc389b40a08ed60b3a468811967a56770dc4fc
Annelise2019 <53711263+Annelise2019@users.noreply.github.com>
2021-05-02 10:46:53 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 params = {}
 
-params['root'] = '/data/nturgbd-skeleton/xsub' # to change according to your own data repo
+params['root'] = '/data/nturgbd-skeleton/xsub' # to change according to your own data path
 params['batchsize'] = 32
 params['numworkers'] = 8
 params['gpu'] =[0,1] # to change according to your own gpu settings
-params['pretrain'] = False #'./checkpoint/bert_model_2020-06-18-09-09-59.pth' 
-params['retrain'] = False #'./checkpoint/bert_classifymodel_2020-06-24-22-54-42.pth'#'./checkpoint/bert_classifymodel_2020-06-17-14-44-48.pth'#'./checkpoint/bert_model_2020-06-06-17-05-13.pth'
+params['retrain'] = False #'./checkpoint/bert_classifymodel_xxxx.pth'
 
 params['lr'] = 1e-3 
 params['weight_decay'] = 1e-6
